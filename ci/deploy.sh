e2eecd05ede32d63ab212d695d8ecd9b2b035ef3
Steve Greenberg <stephen.greenberg@gsa.gov>
2025-07-17 07:08:15 -0600

    set app name via var

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -11,6 +11,7 @@ cf auth
 cf t -o $CF_ORG -s $CF_SPACE
 
 cf push -f source/cf/manifest.yml \
+    --var app_name=${CLAMAV_REST_HOSTNAME} \
     --var docker_username=${CF_DOCKER_USERNAME} \
     --var ecr_repository=${ecr_repository} \
     --var route=${CLAMAV_REST_HOSTNAME}.${CLAMAV_REST_DOMAIN} \
