b9fe2cdd5162237f4230725605a78566da74c60d
Steve Greenberg <stephen.greenberg@gsa.gov>
2025-07-22 06:42:31 -0600

    open a pr on new clamav or golang

---
+++ ci/update-versions.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+clamav_rest_version=$(cat clamav-rest-release/version)
+go_version=$(cat golang-release/version)
+
+pushd source
+    echo "CLAMAV_REST_VERSION=${clamav_rest_version}" > image/args/build-args.conf
+    echo "GO_VERSION=${go_version}" >> image/args/build-args.conf
+    cat image/args/build-args.conf
+    git checkout -b depenedencies
+    git commit -m "update depenedencies" image/args/build-args.conf
+    git push origin dependencies
+popd
