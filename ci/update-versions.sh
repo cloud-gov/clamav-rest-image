6e085d73500fe35a28f612738cb933686dc544e9
Steve Greenberg <stephen.greenberg@gsa.gov>
2025-07-22 08:25:36 -0600

    config git

--- ci/update-versions.sh
+++ ci/update-versions.sh
@@ -3,6 +3,9 @@
 clamav_rest_version=$(cat clamav-rest-release/version)
 go_version=$(curl -s "https://go.dev/VERSION?m=text" | head -n 1)
 
+git config --global user.email "no-reply@cloud.gov"
+git config --global user.name "cg-ci-bot"
+
 pushd source
     echo "CLAMAV_REST_VERSION=${clamav_rest_version}" > image/args/build-args.conf
     echo "GO_VERSION=${go_version}" >> image/args/build-args.conf
