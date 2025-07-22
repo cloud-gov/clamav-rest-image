2f8eb1a62f2b92f6601e79eb01889dd8fc9bf10f
Steve Greenberg <stephen.greenberg@gsa.gov>
2025-07-22 10:20:45 -0600

    trigger based on image dir but use the most recent in the build

--- ci/update-versions.sh
+++ ci/update-versions.sh
@@ -4,9 +4,12 @@ clamav_rest_version=$(cat clamav-rest-release/version)
 go_version=$(curl -s "https://go.dev/VERSION?m=text" | head -n 1)
 
 pushd source
+    gpg --batch --import private.key
+    
     git config --global user.email "no-reply@cloud.gov"
     git config --global user.name "cg-ci-bot"
     git config commit.gpgsign true
+
 
     echo "CLAMAV_REST_VERSION=${clamav_rest_version}" > image/args/build-args.conf
     echo "GO_VERSION=${go_version}" >> image/args/build-args.conf
