4a7c1c3d67b07aa152a368c1db23a9c97a7259b3
Steve Greenberg <stephen.greenberg@gsa.gov>
2025-07-22 07:16:03 -0600

    golang doesn't use releases only tags

--- ci/update-versions.sh
+++ ci/update-versions.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 clamav_rest_version=$(cat clamav-rest-release/version)
-go_version=$(cat golang-release/version)
+go_version=$(cat golang-source/.git/ref)
 
 pushd source
     echo "CLAMAV_REST_VERSION=${clamav_rest_version}" > image/args/build-args.conf
