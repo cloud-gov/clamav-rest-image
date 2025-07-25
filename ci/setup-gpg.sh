37f5a4bda1bb37abdf1837ea192d8f81576fca32
Steve Greenberg <stephen.greenberg@gsa.gov>
2025-07-25 08:17:58 -0600

    sign commits

---
+++ ci/setup-gpg.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+set -e
+
+export GPG_TTY=$(tty)
+
+# setup the gpg-agent to allow presetting a password and enable caching
+mkdir -p ~/.gnupg
+echo "allow-preset-passphrase" > ~/.gnupg/gpg-agent.conf
+echo "default-cache-ttl 120" >> ~/.gnupg/gpg-agent.conf
+echo "max-cache-ttl 240" >> ~/.gnupg/gpg-agent.conf
+
+gpgconf --reload gpg-agent
+
+echo "$GPG_PUBLIC_KEY" > public.key
+gpg --import public.key
+
+echo "$GPG_PRIVATE_KEY" > private.key
+gpg --import --batch private.key
+
+secret_keys=$(gpg --list-secret-keys --with-colons "$GPG_USERNAME <$GPG_EMAIL>")
+
+# labeled as "fpr" 
+gpg_fingerprint=$(echo "$secret_keys" | grep "fpr:" | head -n 1 | awk -F "fpr:*" '{print $2}' | awk -F ":" '{print $1}')
+echo "${gpg_fingerprint}:6:" > ownertrust.txt
+gpg --import-ownertrust < ownertrust.txt
+
+gpg_secret_keyline=$(echo "$secret_keys" | grep "sec")
+
+IFS=":" read -ra gpg_secret_key_details <<< "$gpg_secret_keyline"
+gpg_secret_keyid="${gpg_secret_key_details[5]}"
+# git setup
+git config --global user.email "$GPG_EMAIL"
+git config --global user.name "$GPG_USERNAME"
+git config --global commit.gpgSign true
+git config --global user.signingkey ${gpg_secret_keyid}
+
+# labeled as "grp"
+gpg_keygrip=$(echo "$secret_keys" | grep "grp:" | head -n 1 | awk -F "grp:*" '{print $2}' | awk -F ":" '{print $1}')
+
+/usr/lib/gnupg2/gpg-preset-passphrase -c $gpg_keygrip <<< $GPG_PASSPHRASE
--- ci/update-versions.sh
+++ ci/update-versions.sh
@@ -4,16 +4,12 @@ clamav_rest_version=$(cat clamav-rest-release/version)
 go_version=$(curl -s "https://go.dev/VERSION?m=text" | head -n 1)
 
 pushd source
-    gpg --batch --import private.key
-    
-    git config --global user.email "no-reply@cloud.gov"
-    git config --global user.name "cg-ci-bot"
-    git config commit.gpgsign true
-
+    # gpg setup
+    ci/setup-gpg.sh
 
     echo "CLAMAV_REST_VERSION=${clamav_rest_version}" > image/args/build-args.conf
     echo "GO_VERSION=${go_version}" >> image/args/build-args.conf
     cat image/args/build-args.conf
     git checkout -b depenedencies
-    git commit -m "update depenedencies" image/args/build-args.conf
+    git commit -S -m "update depenedencies" image/args/build-args.conf
 popd
