96a62ff596cd8fce36ecbad269302058b436ace9
Steve Greenberg <stephen.greenberg@gsa.gov>
2025-07-25 09:48:16 -0600

    force fetch to prevent prompt

--- ci/update-versions.sh
+++ ci/update-versions.sh
@@ -15,7 +15,7 @@ pushd source
 
     set -x
 
-    git fetch
+    git fetch -f
     existing_branch=$(git branch --list | grep -w depenedencies)
     if [[ -z "$existing_branch" ]]; then
         git checkout -b depenedencies
