c0a19bce0bb8700f8c9d9203368e039da9d41e81
Steve Greenberg <stephen.greenberg@gsa.gov>
2025-07-25 08:31:38 -0600

    debug

--- ci/setup-gpg.sh
+++ ci/setup-gpg.sh
@@ -10,13 +10,22 @@ echo "allow-preset-passphrase" > ~/.gnupg/gpg-agent.conf
 echo "default-cache-ttl 120" >> ~/.gnupg/gpg-agent.conf
 echo "max-cache-ttl 240" >> ~/.gnupg/gpg-agent.conf
 
+echo "GPG agent configured"
+cat ~/.gnupg/gpg-agent.conf
+
 gpgconf --reload gpg-agent
+
+echo "GPG agent reloaded"
 
 echo "$GPG_PUBLIC_KEY" > public.key
 gpg --import public.key
 
+echo "Public key imported"
+
 echo "$GPG_PRIVATE_KEY" > private.key
 gpg --import --batch private.key
+
+echo "Private key imported"
 
 secret_keys=$(gpg --list-secret-keys --with-colons "$GPG_USERNAME <$GPG_EMAIL>")
 
@@ -24,6 +33,8 @@ secret_keys=$(gpg --list-secret-keys --with-colons "$GPG_USERNAME <$GPG_EMAIL>")
 gpg_fingerprint=$(echo "$secret_keys" | grep "fpr:" | head -n 1 | awk -F "fpr:*" '{print $2}' | awk -F ":" '{print $1}')
 echo "${gpg_fingerprint}:6:" > ownertrust.txt
 gpg --import-ownertrust < ownertrust.txt
+
+echo "Ownertrust updated"
 
 gpg_secret_keyline=$(echo "$secret_keys" | grep "sec")
 
@@ -35,7 +46,11 @@ git config --global user.name "$GPG_USERNAME"
 git config --global commit.gpgSign true
 git config --global user.signingkey ${gpg_secret_keyid}
 
+echo "Git configured"
+
 # labeled as "grp"
 gpg_keygrip=$(echo "$secret_keys" | grep "grp:" | head -n 1 | awk -F "grp:*" '{print $2}' | awk -F ":" '{print $1}')
 
 /usr/lib/gnupg2/gpg-preset-passphrase -c $gpg_keygrip <<< $GPG_PASSPHRASE
+
+echo "Passphrase preset"
