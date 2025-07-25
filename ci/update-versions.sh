4256e8341e392920b509634971cea54bacf154fc
Steve Greenberg <stephen.greenberg@gsa.gov>
2025-07-25 10:39:05 -0600

    try using https for the origin

--- ci/update-versions.sh
+++ ci/update-versions.sh
@@ -17,6 +17,9 @@ pushd source
 
     set -x
 
+
+    git remote set-url origin https://github.com/cloud-gov/clamav-rest-image
+    
     cat .git/config
 
     GIT_SSH_COMMAND="ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" git fetch --all
