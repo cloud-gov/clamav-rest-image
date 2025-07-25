ea540d3d3772086c18a415e55669118e2605e2c8
Steve Greenberg <stephen.greenberg@gsa.gov>
2025-07-25 10:55:34 -0600

    try to use a dedicated resource for the dependencies branch

--- ci/update-versions.sh
+++ ci/update-versions.sh
@@ -5,31 +5,18 @@ set -e
 clamav_rest_version=$(cat clamav-rest-release/version)
 go_version=$(curl -s "https://go.dev/VERSION?m=text" | head -n 1)
 
-pushd source
-    # gpg setup
-    ci/setup-gpg.sh
+# gpg setup
+source/ci/setup-gpg.sh
 
-    branch_name=dependencies
+if [ -d dependencies-branch ]; then
+    cd dependencies-branch
+else 
+    git checkout -b ${branch_name}
+fi
+    
+echo "CLAMAV_REST_VERSION=${clamav_rest_version}" > image/args/build-args.conf
+echo "GO_VERSION=${go_version}" >> image/args/build-args.conf
+cat image/args/build-args.conf
 
-    echo "CLAMAV_REST_VERSION=${clamav_rest_version}" > image/args/build-args.conf
-    echo "GO_VERSION=${go_version}" >> image/args/build-args.conf
-    cat image/args/build-args.conf
+git commit -S -m "update depenedencies" image/args/build-args.conf
 
-    set -x
-
-    git remote set-url origin https://github.com/cloud-gov/clamav-rest-image
-
-    git checkout main
-    cat .git/config
-
-    GIT_SSH_COMMAND="ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" git fetch --all
-    git branch -r --list
-
-    existing_branch=$(git branch -r --list | grep -w "origin/${branch_name}")
-    if [[ -z "$existing_branch" ]]; then
-        git checkout -b ${branch_name}
-    else 
-        git checkout ${branch_name}
-    fi
-    git commit -S -m "update depenedencies" image/args/build-args.conf
-popd
