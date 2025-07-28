2b517f868d9d20919a10a06258ff8aef622b90b5
Steve Greenberg <stephen.greenberg@gsa.gov>
2025-07-28 06:31:43 -0600

    wip - git fun

--- ci/update-versions.sh
+++ ci/update-versions.sh
@@ -5,18 +5,26 @@ set -e
 clamav_rest_version=$(cat clamav-rest-release/version)
 go_version=$(curl -s "https://go.dev/VERSION?m=text" | head -n 1)
 
-# gpg setup
-source/ci/setup-gpg.sh
+pushd source
 
-if [ -d dependencies-branch ]; then
-    cd dependencies-branch
-else 
-    git checkout -b ${branch_name}
-fi
-    
-echo "CLAMAV_REST_VERSION=${clamav_rest_version}" > image/args/build-args.conf
-echo "GO_VERSION=${go_version}" >> image/args/build-args.conf
-cat image/args/build-args.conf
+    # gpg setup
+    ci/setup-gpg.sh
 
-git commit -S -m "update depenedencies" image/args/build-args.conf
+    set -x 
 
+    git pull --rebase
+    branch_name=$(dependencies-branch)
+    dependencies-branch=$(git branch -r list | grep $branch_name)
+    if [ -z $branch_name ]; then
+        git checkout -b $branch_name
+    else 
+        git checkout $branch_name
+    fi
+        
+    echo "CLAMAV_REST_VERSION=${clamav_rest_version}" > image/args/build-args.conf
+    echo "GO_VERSION=${go_version}" >> image/args/build-args.conf
+    cat image/args/build-args.conf
+
+    git commit -S -m "update depenedencies" image/args/build-args.conf
+
+popd
