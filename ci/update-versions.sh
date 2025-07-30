5b0c4dfd55d446c4063d5e4b81ddef066682d881
Steve Greenberg <stephen.greenberg@gsa.gov>
2025-07-30 07:03:03 -0600

    only commit when changes exist

--- ci/update-versions.sh
+++ ci/update-versions.sh
@@ -27,27 +27,32 @@ pushd source
         
     echo "CLAMAV_REST_VERSION=${clamav_rest_version}" > image/args/build-args.conf
     echo "GO_VERSION=${go_version}" >> image/args/build-args.conf
-    cat image/args/build-args.conf
 
-    git commit -S -m "update depenedencies" image/args/build-args.conf
-    git push origin $branch_name
+    # Only commit if there are changes
+    git_diff=$(git diff image/args/build-args.conf)
+    if [[ -z "$git_diff" ]]; then
+        echo "No changes to image/args/build-args.conf. Exiting..."
+    else 
+        git commit -S -m "update depenedencies" image/args/build-args.conf
+        git push origin $branch_name
 
-    # Open a PR if one does not exist.
-    existing_pr_count=$(gh pr list --author "@cg-ci-bot" --label "dependencies" --json "author" | jq 'length')
-    if [[ 0 == "$existing_pr" ]]; then
+        # Open a PR if one does not exist.
+        existing_pr_count=$(gh pr list --author "@cg-ci-bot" --label "dependencies" --json "author" | jq 'length')
+        if [[ 0 == "$existing_pr" ]]; then
 
-        # TODO Open a PR w/ GH_TOKEN & GH_REPO
-        GH_REPO=$(cat .git/config| grep url | head -n 1 | awk -F "url = " '{print $2}')
-        
-        body="
-        ## Changes proposed in this pull request\n \
-        - Dependencies updated in image/args/build-args.conf.\n\n \
-        ## Security considerations\n \
-        Updates are good.\n \
-        "
+            # TODO Open a PR w/ GH_TOKEN & GH_REPO
+            GH_REPO=$(cat .git/config| grep url | head -n 1 | awk -F "url = " '{print $2}')
+            
+            body="
+            ## Changes proposed in this pull request\n \
+            - Dependencies updated in image/args/build-args.conf.\n\n \
+            ## Security considerations\n \
+            Updates are good.\n \
+            "
 
-        gh pr create --title "Dependencies updated" --body "$body" --label "dependencies"
-
+            gh pr create --title "Dependencies updated" --body "$body" --label "dependencies"
+        else
+            echo "PR already exists. Exiting..."
+        fi
     fi
-
 popd
