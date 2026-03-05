d114541933491f811e0924f355fbbcae95f48989
Steve Greenberg <stephen.greenberg@gsa.gov>
2026-03-05 08:54:35 -0700

    pass allowed range

--- ci/deploy-nginx.sh
+++ ci/deploy-nginx.sh
@@ -8,7 +8,7 @@ cf auth
 cf t -o $CF_ORG -s $CF_SPACE
 
 pushd source/nginx
-    cf push -f manifest.yml 
+    cf push -f manifest.yml -var allowed_ip_range="${ALLOWED_IP_RANGE}"
 popd
 
 cf add-network-policy clamav-rest clamav-rest-endpoint --protocol tcp --port 8080
