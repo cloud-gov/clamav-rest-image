d7232aee8cd01df30d443cc49e6a4ba4a8ed2f78
Steve Greenberg <stephen.greenberg@gsa.gov>
2026-03-05 08:23:52 -0700

    be sure to push nginx.conf

--- ci/deploy-nginx.sh
+++ ci/deploy-nginx.sh
@@ -7,7 +7,9 @@ cf auth
 
 cf t -o $CF_ORG -s $CF_SPACE
 
-cf push -f source/nginx/manifest.yml 
+pushd source/nginx
+    cf push -f manifest.yml 
+popd
 
 cf add-network-policy clamav-rest clamav-rest-endpoint --protocol tcp --port 8080
 
