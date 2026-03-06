a707e5c9004c13750f120816e79a0635caedfb43
Steve Greenberg <stephen.greenberg@gsa.gov>
2026-03-06 07:37:10 -0700

    include tooling egress range as requests leave before coming back in bounds

--- ci/deploy-nginx.sh
+++ ci/deploy-nginx.sh
@@ -8,7 +8,12 @@ cf auth
 cf t -o $CF_ORG -s $CF_SPACE
 
 pushd source/nginx
-    cf push -f manifest.yml --var allowed_ip_range="${ALLOWED_IP_RANGE}" --var concourse_garden_ip_range1="${CONCOURSE_GARDEN_IP_RANGE1}" --var concourse_garden_ip_range2="${CONCOURSE_GARDEN_IP_RANGE2}"
+    cf push -f manifest.yml \
+        --var allowed_ip_range="${ALLOWED_IP_RANGE}" \
+        --var tooling_egress1="${TOOLING_EGRESS1}" \
+        --var tooling_egress2="${TOOLING_EGRESS2}" \
+        --var concourse_garden_ip_range1="${CONCOURSE_GARDEN_IP_RANGE1}" \
+        --var concourse_garden_ip_range2="${CONCOURSE_GARDEN_IP_RANGE2}"
 popd
 
 cf add-network-policy clamav-rest clamav-rest-endpoint --protocol tcp --port 8080
