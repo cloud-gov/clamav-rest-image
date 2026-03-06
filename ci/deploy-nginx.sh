00cd6e3680627570a09d796d89e5ea494e3ef05b
Steve Greenberg <stephen.greenberg@gsa.gov>
2026-03-06 06:39:27 -0700

    use x-forwarded-for header for real ip

--- ci/deploy-nginx.sh
+++ ci/deploy-nginx.sh
@@ -8,7 +8,7 @@ cf auth
 cf t -o $CF_ORG -s $CF_SPACE
 
 pushd source/nginx
-    cf push -f manifest.yml --var concourse_garden_ip_range1="${CONCOURSE_GARDEN_IP_RANGE1}" --var concourse_garden_ip_range2="${CONCOURSE_GARDEN_IP_RANGE2}"
+    cf push -f manifest.yml --var allowed_ip_range="${ALLOWED_IP_RANGE}" --var concourse_garden_ip_range1="${CONCOURSE_GARDEN_IP_RANGE1}" --var concourse_garden_ip_range2="${CONCOURSE_GARDEN_IP_RANGE2}"
 popd
 
 cf add-network-policy clamav-rest clamav-rest-endpoint --protocol tcp --port 8080
