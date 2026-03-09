4bcbbe436475cf9498953e99b23dd71e87eb1792
Steve Greenberg <stephen.greenberg@gsa.gov>
2026-03-09 06:53:35 -0600

    correct param

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -11,7 +11,7 @@ cf auth
 cf t -o $CF_ORG -s $CF_SPACE
 
 cf push -f source/cf/manifest.yml \
-    -i CLAMAV_REST_INSTANCES \
+    -i $CLAMAV_REST_INSTANCES \
     --var app_name=${CLAMAV_REST_HOSTNAME} \
     --var docker_username=${CF_DOCKER_USERNAME} \
     --var ecr_repository=${ecr_repository} \
